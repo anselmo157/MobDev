3f31fb4154efbda15e582e5d01825ad882791ad6
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-05-16 17:03:32 -0300

    Created message container

---
+++ lib/ui/widgets/message_container.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MessageContainer extends StatelessWidget {
+  const MessageContainer({super.key});
+
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
