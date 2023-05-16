2f1ede7e4d915761038a9da8bcafbf945858bbc2
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-05-16 16:07:27 -0300

    Remove banner of debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(title: 'WhyZap'),
