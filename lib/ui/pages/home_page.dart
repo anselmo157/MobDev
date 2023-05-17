b2be2cd9e8dd9485a1b84599d4d0d29480b2df14
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-05-17 13:32:51 -0300

    Update message container and add to home page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_test/ui/widgets/message_container.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,28 +7,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          centerTitle: true,
-          title: const Text(
-            'WhyZap',
-            style: TextStyle(color: Colors.white),
-          ),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(12.0),
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/user-page');
-              },
-              icon: const Icon(Icons.person, color: Colors.white),
-            ),
-          ],
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        centerTitle: true,
+        title: const Text(
+          'WhyZap',
+          style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.yellow[50],
-        body: Container());
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(12.0),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/user-page');
+            },
+            icon: const Icon(Icons.person, color: Colors.white),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.yellow[50],
+      body: const MessageContainer(message: 'Olá, mundo!'),
+    );
   }
 }
--- lib/ui/widgets/message_container.dart
+++ lib/ui/widgets/message_container.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 
 class MessageContainer extends StatelessWidget {
-  const MessageContainer({super.key});
+  final String message;
 
+  const MessageContainer({
+    required this.message,
+    super.key,
+  });
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        const CircleAvatar(
+          child: Text('A'),
+        ),
+        const SizedBox(
+          width: 8.0,
+        ),
+        Container(
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8.0,
+            vertical: 4.0,
+          ),
+          child: Text(message),
+        ),
+      ],
+    );
   }
 }
